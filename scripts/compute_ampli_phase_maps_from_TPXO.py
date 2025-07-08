755d8d80dc4c2f1e2a692fb53e4bd825067eaa20
Hplombat <hugo.plombat@orange.fr>
2025-07-08 16:44:21 -0700

    % Amplitude and Phase map using 5 years time series and scripts to generate them

--- scripts/compute_ampli_phase_maps_from_TPXO.py
+++ scripts/compute_ampli_phase_maps_from_TPXO.py
@@ -33,7 +33,7 @@ import time
 model_list = sorted(pyTMD.io.model.ocean_elevation())
 
 # get model parameters
-model = pyTMD.io.model('/efs_ecco/hplombat/TMD', #TOCHANGE: path of the TPXO data
+model = pyTMD.io.model('../../TMD', #TOCHANGE: path of the TPXO data
                        compressed= False
                        ).elevation('TPXO9.1')
 
@@ -43,37 +43,28 @@ if model.format in ('OTIS','ATLAS-compact','TMD3'):
     xi,yi,hz,mz,iob,dt = pyTMD.io.OTIS.read_otis_grid(model.grid_file)
 
 
-def amp_phase(longit, latit):
+def make_map():  
     '''
-     Returns amplitude and phase of tidal constituents, bathymetry of tidal model, and list of tidal constituents
-    '''
-    amp,ph,D,c = pyTMD.io.OTIS.extract_constants(longit,latit, model.grid_file, model.model_file, 4326,
-                                                 type='z',
-                                                 method= 'bilinear', 
-                                                 grid='OTIS')
-    return amp, ph 
-
-
-
-def make_map():                                                                                                                                                                            # Returns amplitude and phase of tidal constituents, bathymetry of tidal model, and list of tidal constituents
-    longitude = np.arange(0,361)
-    latitude = np.arange(-90,91)
-    lon2d,lat2d = np.meshgrid(longitude,latitude) # We want to treat the whole map at once, but the function only takes longitude and latitude arrays of equal size
+    Returns amplitude and phase of tidal constituents, bathymetry of tidal model, and list of tidal constituents
+    '''                                                                                                                                                                          # Returns amplitude and phase of tidal constituents, bathymetry of tidal model, and list of tidal constituents
+    longitude = np.arange(-180,180)
+    latitude = np.arange(-90,90)
+    lon2d,lat2d = np.meshgrid(longitude,latitude) # We want to treat the whole map at once, but the function only takes 
+    #longitude and latitude arrays of equal size
     lonflat = lon2d.ravel() #packs 2d array in 1d array
     latflat= lat2d.ravel() 
-    amp,ph,D,components = pyTMD.io.OTIS.extract_constants(lonflat,latflat, model.grid_file, model.model_file, 4326,
+    amp,ph,D,components = pyTMD.io.OTIS.extract_constants(lonflat,latflat, model.grid_file, model.model_file, projection=4326,
                                                  type='z',
                                                  method='bilinear',
                                                  grid='OTIS')
     ny, nx = lon2d.shape     # e.g. (180, 180)
-    n_constituents = amp.shape[1]  # should be 15
+    n_constituents = amp.shape[1]  # should be 15 constituents
     # Now reshape into (ny, nx, n_constituents)
     amp_grid = amp.reshape(ny, nx, n_constituents)
     ph_grid  = ph.reshape(ny, nx, n_constituents)
-    print(components)
     for c in range(len(components)): #CHANGE the writing path 
-        np.savetxt('./test/ampli_ll_txpo_'+str(components[c])+'.txt',amp_grid[:,:,c])
-        np.savetxt('./test/ph_ll_txpo_'+str(components[c])+'.txt',ph_grid[:,:,c])
+        np.savetxt('./maps_tpxo_long_180/ampli_ll_txpo_'+str(components[c])+'.txt',amp_grid[:,:,c])
+        np.savetxt('./maps_tpxo_long_180/ph_ll_txpo_'+str(components[c])+'.txt',ph_grid[:,:,c])
 make_map()
 
 
--- scripts/compute_ampli_phase_maps_from_mitgcm_output.py
+++ scripts/compute_ampli_phase_maps_from_mitgcm_output.py
@@ -18,26 +18,39 @@ from datetime import datetime, timedelta
 import pyTMD
 import xarray as xr
 
-data_dir ='/efs_ecco/hvanderz/export_tides' #CHANGE: directory of ETAN files
-grid_dir ='/efs_ecco/hvanderz/r5/WORKINGDIR/ECCOV4/release5_ToTs/run' #CHANGE: directory of grid files (e.g. XC.data)
+import warnings
+
+warnings.filterwarnings(
+    "ignore", 
+    category=FutureWarning, 
+    message=".*decode_timedelta will default to False.*"
+)
+
+data_dir ='/home/plombat/Documents/Postdoc/ECCO/ETAN_hourly' #CHANGE: directory of ETAN files
+grid_dir ='/home/plombat/Documents/Postdoc/ECCO/grid_1deg' #CHANGE: directory of grid files (e.g. XC.data)
 ds_llc = open_mdsdataset(data_dir, 
                         grid_dir=grid_dir, 
-                         prefix={'ETAN'},
+                         prefix={'ETAN_hourly'},
                          geometry="llc") #non-native grid
 # Rename to be consistent with ECCOV4-py
 ds_llc = ds_llc.rename({'face':'tile'})
 
 #We need to change the format of time to make it relative to the first day.
-ds_llc['time'] = xr.cftime_range(start='1992-01-01T13:00:00', periods=ds_llc.dims['time'], freq='H')
-ds_llc = ds_llc.unify_chunks()
-ds_clm = ds_llc.groupby('time.month')#.mean('time')#ds_llc.mean('time')
-ds_llc_anom = ds_llc.groupby('time.month') - ds_clm #Remove monthly mean of the signal
+ds_llc['time'] = xr.date_range(start='1992-01-01T13:00:00', periods=ds_llc.sizes['time'], freq='h')#,use_cftime=True) #dims
 
-n_files = len(ds_llc_anom.time)              # number of data files                                                                                                                    
+n_files =len(ds_llc['time'])
+
+
+CLIM = False
+
+if CLIM: # Remove monthly climatology
+    ds_llc = ds_llc.unify_chunks()
+    ds_clm = ds_llc.groupby('time.month').mean('time')
+    ds_llc_anom = ds_llc.groupby('time.month') - ds_clm #Remove monthly mean of the signal
+
 start_time = '1992-01-01 13:00:00'       # starting date of the simulation   
 freq = 'h'                   # time frequency (e.g. hourly)                                                                  
 time_eta = pd.date_range(start=start_time, periods=n_files, freq=freq)
-
 epoch = datetime(1992, 1, 1, 0, 0, 0) 
 dates = np.array([(ti.to_pydatetime() - epoch).total_seconds() / 86400.0 for ti in time_eta])
 
@@ -83,8 +96,14 @@ def loop_tiles(ds):
         amp_2d = amp_allconsts.reshape(amp_allconsts.shape[0], -1)#Flattens a 3d array (constituent,i,j) in a 2d array before saving it.
         pha_2d = pha_allconsts.reshape(pha_allconsts.shape[0], -1)
         #Saves 13 amplitude and phase files, one per tile.
-        np.savetxt('./new_maps_mitgcm_1year/amp_comsts_'+str(tilenum)+'.txt', amp_2d) #CHANGE SAVE PATH
-        np.savetxt('./new_maps_mitgcm_1year/pha_consts_'+str(tilenum)+'.txt', pha_2d)
+        if CLIM:
+            np.savetxt('./maps_mitgcm_5years_clim/amp_consts_'+str(tilenum)+'.txt', amp_2d) #CHANGE SAVE PATH
+            np.savetxt('./maps_mitgcm_5years_clim/pha_consts_'+str(tilenum)+'.txt', pha_2d)
+        else: 
+            np.savetxt('./maps_mitgcm_5years_noclim/amp_consts_'+str(tilenum)+'.txt', amp_2d) #CHANGE SAVE PATH
+            np.savetxt('./maps_mitgcm_5years_noclim/pha_consts_'+str(tilenum)+'.txt', pha_2d)
 
-loop_tiles(ds_llc)
-
+if CLIM:
+    loop_tiles(ds_llc_anom)
+else: 
+    loop_tiles(ds_llc)
